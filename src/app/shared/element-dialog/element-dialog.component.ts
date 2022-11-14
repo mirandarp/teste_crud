3497f322dad75e55ee06ec17015cb2d112c4da9e
mirandarp <welissonmiranda30@gmail.com>
2022-11-14 12:02:24 -0300

    Primeiro

---
+++ src/app/shared/element-dialog/element-dialog.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { PeriodicElement } from 'src/app/views/home/home.component';
+import { Inject} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-element-dialog',
+  templateUrl: './element-dialog.component.html',
+  styleUrls: ['./element-dialog.component.scss']
+})
+export class ElementDialogComponent implements OnInit {
+  element!: PeriodicElement;
+  isChange!: boolean;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) 
+    public data: PeriodicElement,
+    public dialogRef: MatDialogRef<ElementDialogComponent>,
+   
+  ) {}
+
+  
+   
+
+  ngOnInit(): void {
+    if (this.data.position !=null)
+    this.isChange=true;
+    else{
+      this.isChange = false;
+    }
+  }
+  onCancel(): void {
+    this.dialogRef.close();
+}
+
+
+}
